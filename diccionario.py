c1bb63d16493762201ed03e1267462002307deb7
Cesar Najera <164533997+Cesar-Najera-1068@users.noreply.github.com>
2024-09-06 12:09:45 -0600

    Add files via upload

---
+++ diccionario.py
@@ -0,0 +1,16 @@
+Libros =	{
+  "Titulo": "El principito",
+  "Autor": "Antoine",
+  "Fecha": 1943
+}
+print(Libros)
+
+
+Libros =	{
+  "Titulo": "Principito",
+  "Autor": "Antoine",
+  "Año": 1943
+}
+for datos in Libros:
+  print(datos)
+
---
+++ misconjuntos.py
@@ -0,0 +1,15 @@
+sabores = {"Uva", "Fresa", "Sangre de trigre"}
+print(sabores)
+
+
+sabores = {"Uva", "Fresa", "Sangre de trigre"}
+sabores.add("Naranja")
+print(sabores)
+
+
+sabores = {"Uva", "Fresa", "Sangre de trigre"}
+sabores.remove("Fresa")
+
+
+for disponibles in sabores:
+    print(sabores)
